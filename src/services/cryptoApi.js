07f8a3f3886f4a9590c802947ba92aa026d501e7
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-20 08:59:09 +0100

    querying the crypto api with redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '87ff37c9a2msh4741787aebf3a53p1c0a24jsn5178b80e05cd'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+// create a utility function that will add the headers and url to our call
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
